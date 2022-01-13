d735d31a23c20a653edba726c10fe99480b256c1
Daniel Zhang <dzhangwh@gmail.com>
2022-01-13 10:01:30 -0500

    first

---
+++ src/00-objectball.js
@@ -0,0 +1,15 @@
+function gameObject () { 
+    const teams = {
+    
+    home: {teamName: , 
+        colors: [], 
+        players: "Lebron" {number: , shoe: , points: ,
+        rebounds: , assists:, steals: , blocks: , slamDunks: } },
+    
+    
+    away: {teamName: , 
+        colors: [], 
+        players: {number: , shoe: , points: ,
+        rebounds: , assists:, steals: , blocks: , slamDunks: } },}
+
+};
